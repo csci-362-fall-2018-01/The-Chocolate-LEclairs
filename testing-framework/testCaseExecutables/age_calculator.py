bdaa294c7cb17986bd9e50b46d19b7fe2756de20
skiffaw <skiffaw@g.cofc.edu>
2018-11-11 21:50:12 -0500

    setup testcase

---
+++ testing-framework/testCaseExecutables/age_calculator.py
@@ -0,0 +1,9 @@
+import unittest
+import jarabe.intro.agepicker as agepick
+
+class TestAgeCalculation(unittest.TestCase):
+	def test_age_calc(self):
+		print('hello')
+
+if __name__ == '__main__':
+	unittest.main();
