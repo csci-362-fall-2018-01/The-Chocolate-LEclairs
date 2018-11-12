fe1ddf9cf0fb930d552431c8c7059ef55f0d787c
skiffaw <skiffaw@g.cofc.edu>
2018-11-12 00:15:10 -0500

    cleaned up runAllTests

--- testing-framework/scripts/runAllTests.py
+++ testing-framework/scripts/runAllTests.py
@@ -5,8 +5,40 @@ import subprocess
 ls_output = subprocess.check_output("ls ../testCaseExecutables/", shell = True)
 test_files = ls_output.split()
 
+os.system("mv ../temp/output.log ../temp/output2.log")
 os.system("date >> ../temp/output.log")
 
 for test_file_name in test_files:
     os.system("echo \"\" >> ../temp/output.log")
     subprocess.check_output("python -W ignore ../testCaseExecutables/" + test_file_name + " 2>> ../temp/output.log", shell=True)
+
+f = open("../temp/output.log", "r")
+lines = f.readlines()
+f.close()
+
+removenext = False
+f = open("../temp/output.log", "w")
+for line in lines:
+    if not line.startswith(".") and not line.startswith("-"):
+        if line.startswith("R"):
+            removenext = True
+            f.write(line)
+        elif removenext == True:
+            removenext = False
+        else:
+            f.write(line)
+f.close()
+
+moved_file = open("../temp/output2.log")
+
+first_line = True
+for line in moved_file:
+    if first_line:
+        os.system("echo \"\n\n\n\n\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" + line[:-1] + "\" >> ../temp/output.log")
+    else:
+        os.system("echo \"" + line[:-1] + "\" >> ../temp/output.log")
+    first_line = False
+
+moved_file.close()
+
+os.system("rm ../temp/output2.log")
