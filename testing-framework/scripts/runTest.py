a548cc9aa4e70878a542d9313da6f4aa45d9699e
skiffaw <skiffaw@g.cofc.edu>
2018-11-11 23:53:04 -0500

    updated individual test case file

--- testing-framework/scripts/runTest.py
+++ testing-framework/scripts/runTest.py
@@ -1,4 +1,8 @@
+import os
 import subprocess
 
+os.system("date >> ../temp/output.log")
+
 test_file_name = raw_input()
+os.system("echo \"\" >> ../temp/output.log")
 subprocess.check_output("python -W ignore ../testCaseExecutables/" + test_file_name + " 2>> ../temp/output.log", shell=True)
