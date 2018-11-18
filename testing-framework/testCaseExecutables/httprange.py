178ce5736f992fb4c1252c5d00c1581f0bb006e9
billingsbt@g.cofc.edu <billingsbt@g.cofc.edu>
2018-11-17 19:25:24 -0500

    Add Second HTTPRange Test Case

--- testing-framework/testCaseExecutables/httprange.py
+++ testing-framework/testCaseExecutables/httprange.py
@@ -3,13 +3,26 @@ import unittest
 import jarabe.util.httprange as httprange
 
 class TestHTTPRange(unittest.TestCase):
+    def test_open(self):
+        for line in open("../testCases/httprange.txt"):
+            if not line.startswith("#"):
+                url = line[:line.index(",")]
+                try:
+                    hr = httprange.open(url)
+                    self.assertEquals(1, 1)
+                except:
+                    self.fail()
+
     def test_size(self):
         for line in open("../testCases/httprange.txt"):
             if not line.startswith("#"):
                 url = line[:line.index(",")]
                 size = line[line.index(",") + 1:-1]
-                hr = httprange.open(url)
-                self.assertEqual(int(hr.size()), int(size))
+                try:
+                    hr = httprange.open(url)
+                    self.assertEquals(int(hr.size()), int(size))
+                except:
+                    self.fail()
 
 if __name__ == '__main__':
     unittest.main()
